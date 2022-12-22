8d176a5ebc8a224da89c082e63ce7712dfa2027a
n101661 <n101661@gmail.com>
2022-12-22 23:20:39 +0800

    return pointer value for Get function from database

--- database/services.go
+++ database/services.go
@@ -5,35 +5,35 @@ import "github.com/n101661/maney/database/models"
 type UserService interface {
 	Create(models.User) error
 	Update(models.User) error
-	Get(id string) (models.User, error)
+	Get(id string) (*models.User, error)
 }
 
 type AccountService interface {
 	Create(userOID uint64, account models.AssetAccount) error
 	Update(userOID uint64, account models.AssetAccount) error
 	Delete(userOID, accountOID uint64) error
-	Get(userOID, accountOID uint64) (models.AssetAccount, error)
+	Get(userOID, accountOID uint64) (*models.AssetAccount, error)
 }
 
 type CategoryService interface {
 	Create(userOID uint64, category models.Category) error
 	Update(userOID uint64, category models.Category) error
 	Delete(userOID, categoryOID uint64) error
-	Get(userOID, categoryOID uint64) (models.Category, error)
+	Get(userOID, categoryOID uint64) (*models.Category, error)
 }
 
 type ShopService interface {
 	Create(userOID uint64, shop models.Shop) error
 	Update(userOID uint64, shop models.Shop) error
 	Delete(userOID, shopOID uint64) error
-	Get(userOID, shopOID uint64) (models.Shop, error)
+	Get(userOID, shopOID uint64) (*models.Shop, error)
 }
 
 type FeeService interface {
 	Create(userOID uint64, fee models.Fee) error
 	Update(userOID uint64, fee models.Fee) error
 	Delete(userOID, feeOID uint64) error
-	Get(userOID, feeOID uint64) (models.Fee, error)
+	Get(userOID, feeOID uint64) (*models.Fee, error)
 }
 
 type DailyItemService interface {
